ee41850f9cfe21929a4c3d8bbb2137d7b17e4b8e
CHEN Xing <cxcxcxcx@gmail.com>
2019-06-01 15:20:04 -0700

    Adds Biaobei explicitly

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import argparse
 import os
 from multiprocessing import cpu_count
 from tqdm import tqdm
-from datasets import blizzard, ljspeech, thchs30
+from datasets import blizzard, ljspeech, thchs30, biaobei
 from hparams import hparams
 
 
@@ -30,6 +30,14 @@ def preprocess_thchs30(args):
   write_metadata(metadata, out_dir)
 
 
+def preprocess_biaobei(args):
+  in_dir = os.path.join(args.base_dir, 'data_biaobei')
+  out_dir = os.path.join(args.base_dir, args.output)
+  os.makedirs(out_dir, exist_ok=True)
+  metadata = biaobei.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
+  write_metadata(metadata, out_dir)
+
+
 def write_metadata(metadata, out_dir):
   with open(os.path.join(out_dir, 'train.txt'), 'w', encoding='utf-8') as f:
     for m in metadata:
@@ -45,7 +53,7 @@ def main():
   parser = argparse.ArgumentParser()
   parser.add_argument('--base_dir', default=os.path.expanduser('.'))
   parser.add_argument('--output', default='training')
-  parser.add_argument('--dataset', default='thchs30', choices=['blizzard', 'ljspeech', 'thchs30'])
+  parser.add_argument('--dataset', default='thchs30', choices=['blizzard', 'ljspeech', 'thchs30', 'biaobei'])
   parser.add_argument('--num_workers', type=int, default=cpu_count())
   args = parser.parse_args()
   if args.dataset == 'blizzard':
@@ -54,6 +62,8 @@ def main():
     preprocess_ljspeech(args)
   elif args.dataset == 'thchs30':
     preprocess_thchs30(args)
+  elif args.dataset == 'biaobei':
+    preprocess_biaobei(args)
 
 
 if __name__ == "__main__":
